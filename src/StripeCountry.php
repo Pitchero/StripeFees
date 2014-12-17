0ce161d6e40b293cc70f41b742d7cdd0c7ba868b
Jon Milsom <j.milsom@pitchero.com>
2014-12-17 18:01:41 +0000

    updated class comments and author

--- src/StripeCountry.php
+++ src/StripeCountry.php
@@ -1,15 +1,13 @@
 <?php
 
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Class to define the structure of each Stripe country
  */
 
 /**
  * Description of StripeCountry
  *
- * @author jonmilsom
+ * @author Jon Milsom
  */
 class StripeCountry {
 		
--- src/StripeCountryFeeStructure.php
+++ src/StripeCountryFeeStructure.php
@@ -1,15 +1,13 @@
 <?php
 
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Class to define the structure of Stripe fees
  */
 
 /**
  * Description of StripeCountryFeeStructure
  *
- * @author jonmilsom
+ * @author Jon Milsom
  */
 class StripeCountryFeeStructure {
 	
